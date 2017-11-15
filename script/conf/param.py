12b452ced893ccb109368e92beae87e85906abd4
shitong <zzzzzcoder@gmail.com>
2017-11-15 08:50:30 -0800

    Adjust IP address

--- script/conf/param.py
+++ script/conf/param.py
@@ -141,4 +141,4 @@ GENERATE_DIFF_STAT = False
 
 DUMMY_LOG_RECORD = '_http://dummy.com/ IF x888y888o888'
 
-DISPATCHER_HOST = 'ec2-52-53-162-48.us-west-1.compute.amazonaws.com'
+DISPATCHER_HOST = 'ec2-54-183-178-81.us-west-1.compute.amazonaws.com'
